a49c8e60e63c5702276ca9894e130a9baac7fdb6
Abu Naim <abunaim@Abus-Air-2.lan>
2024-05-23 23:23:12 +0600

    success

--- backend/settings.py
+++ backend/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGIN = [
     "jwt-authentication-i2yh.onrender.com",
